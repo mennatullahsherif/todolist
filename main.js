25e4a6e42cc83dcc4d06d400639de7c4a386cd74
mennatullahsh <mennatullahsherif75@gmail.com>
2024-12-03 23:42:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const inputbox = document.querySelector("#inputBox");
+const listcontainer = document.querySelector("#listContainer");
+
+function addBt() {
+  if (inputbox.value==='') {
+    alert("You must write something!");
+  }
+  else {
+    let li = document.createElement("li");
+    li.innerHTML=inputbox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+  }
+  inputbox.value="";
+}
+listcontainer.addEventListener("click", function(e){
+  if (e.target.tagName==="LI") {
+    e.target.classList.toggle("checked");
+  }
+  else if (e.target.tagName==="SPAN"){
+    e.target.parentElement.remove();
+  }
+}, false);
